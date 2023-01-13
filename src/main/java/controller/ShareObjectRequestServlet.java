1b3198d304873a75e370946c817168350beaa85c
chp320 <chp320@naver.com>
2023-01-14 01:23:31 +0900

    mvc패턴 기초

--- src/main/java/controller/ShareObjectRequestServlet.java
+++ src/main/java/controller/ShareObjectRequestServlet.java
@@ -1,18 +1,19 @@
+package controller;
+
+import model.vo.CountVO;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "ShareObjectRequestServlet", value = "/ShareObjectRequestServlet")
+@WebServlet("/requestshare")
 public class ShareObjectRequestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        CountVO vo = new CountVO();
+        vo.setNumber(10);
+        request.setAttribute("requestobj", vo);
+        request.getRequestDispatcher("/WEB-INF/view/requestView.jsp").forward(request, response);
     }
 }
--- src/main/webapp/WEB-INF/view/requestView.jsp
+++ src/main/webapp/WEB-INF/view/requestView.jsp
@@ -1,11 +1,23 @@
 <%@ page language="java" contentType="text/html;charset=UTF-8" %>
+<%@ page import="model.vo.CountVO" %>
 <!DOCTYPE html>
-<html> 
-	<head> 
-    	<meta charset="utf-8">
-<title>Insert title here</title>
-	</head> 
-	<body>
-
-	</body>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>request scope</title>
+</head>
+<body>
+<%--	<h2>requestView.jsp</h2>--%>
+    <h2>request scope 공유</h2>
+    <hr>
+    <%
+        CountVO vo = (CountVO) request.getAttribute("requestobj");
+    %>
+    <h3>추출된 카운트 값(스크립트) : <%= vo.getNumber() %></h3>
+    <hr>
+    <jsp:useBean id="requestobj" class="model.vo.CountVO" scope="request" />
+    <h3>추출된 카운트 값(액션 태그) : <jsp:getProperty name="requestobj" property="number"/></h3>
+    <hr>
+    <h3>추출된 카운트 값(EL) : ${ requestScope.requestobj.number }</h3>
+</body>
 </html>
